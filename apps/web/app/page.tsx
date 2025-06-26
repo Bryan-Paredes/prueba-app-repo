6dadba131b6ad73b9ae40305e329acaf290ae0ca
Bryan Paredes <bryansparedes2@gmail.com>
2025-06-25 20:06:21 -0600

    initial commit

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,14 @@
-import { Button } from "@workspace/ui/components/button"
+import { LoginForm } from "@/components/login-form";
+import { Button } from "@workspace/ui/components/button";
 
 export default function Page() {
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World</h1>
-        <Button size="sm">Button</Button>
+        <h1 className="text-2xl font-bold text-red-600">Hello World</h1>
+        <LoginForm />
       </div>
+      <Button variant={"destructive"}>Este es un Botton</Button>
     </div>
-  )
+  );
 }
